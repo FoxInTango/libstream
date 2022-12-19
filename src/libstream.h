19b148caee9d88b414d5e27f64154a1a7187814e
Jack.WSL <foxintango@yeah.net>
2022-12-19 19:38:33 +0800

    alpine

---
+++ src/libstream.h
@@ -0,0 +1,4 @@
+#ifndef _LIB_STREAM_H_
+#define _LIB_STREAM_H_
+#include "stream.h"
+#endif
---
+++ src/stream.h
@@ -0,0 +1,10 @@
+#ifndef _STREAM_H_
+#define _STREAM_H_
+#include <libcpp/libcpp.h>
+
+EXTERN_C_BEGIN
+namespaceBegin(foxintango)
+
+namespaceEnd
+EXTERN_C_END
+#endif
