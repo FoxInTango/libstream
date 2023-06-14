58cda8220094d2548420ae6f7b8fca2d45dd0cd8
Jack.WSL <foxintango@yeah.net>
2023-06-14 22:25:36 +0800

    alpine

--- src/stream.h
+++ src/stream.h
@@ -1,7 +1,8 @@
 #ifndef _STREAM_H_
 #define _STREAM_H_
+#include <liburl/liburl.h>
+#include <libstring/libstring.h>
 #include <libcpp/libcpp.h>
-
 EXTERN_C_BEGIN
 namespaceBegin(foxintango)
 
@@ -9,12 +10,15 @@ class foxintangoAPI Stream
 {
 public:
     Stream();
+    Stream(const URL& url);
     virtual ~Stream();
 public:
     virtual bool readable();
     virtual bool writeable();
     virtual int read(const char* buffer,const unsigned int& size);
     virtual int write(const char* buffer,const unsigned int& size);
+public:
+    virtual Unicode operator [](const Index& index);
 };
 namespaceEnd
 EXTERN_C_END
